539ec9b5d43246e64d303847f1a89576ca299c09
@Adriell <adriel.gomes@nepen.org.br>
2024-04-29 22:49:50 -0300

    save last results

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ from runs import KNNRuns,DMCRuns,BayesianGaussianDiscriminantRuns
 
 
 def main():
-    BayesianGaussianDiscriminantRuns(0)
-    # for i in range(5):
-    #     BayesianGaussianDiscriminantRuns(i)
+    for i in range(5):
+        BayesianGaussianDiscriminantRuns(i)
 if __name__ == "__main__":
     main()
